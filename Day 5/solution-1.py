675b03d10be50f157c513c0cfa486e3d1f5327eb
Nick Frichette <frichetten@gmail.com>
2017-12-05 08:05:00 -0600

    Added Day 5

---
+++ Day 5/solution-1.py
@@ -0,0 +1,20 @@
+# First, lets load a list with the values
+data = []
+with open('input2.txt','r') as f:
+    for line in f:
+        data.append(int(line[:-1]))
+
+steps = 0
+bounds = len(data)
+i = 0
+while i < bounds or i >= 0:
+    try:
+        n = data[i]
+        data[i] += 1
+        i += n
+        steps += 1
+    except:
+        print steps
+        exit()
+
+    
